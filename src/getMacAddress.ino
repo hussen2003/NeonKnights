f0ad5d11c90df7cd35d540e048b6bd58e970a8da
hussen2003 <hussenpremier03@gmail.com>
2024-04-10 01:28:29 -0500

    dev mac address

--- src/getMacAddress.ino
+++ src/getMacAddress.ino
@@ -7,8 +7,8 @@ void setup()
   Serial.println(WiFi.macAddress());
 }
 // dev1 MAC Address: E4:65:B8:76:88:B0
-// dev2 MAC Address: MAC Address: FC:B4:67:72:7C:94
+// dev2 MAC Address: FC:B4:67:72:7C:94
 void loop()
 {
-  // Do Nothing
+
 }
