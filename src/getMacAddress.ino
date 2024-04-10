e2a5bbf11b4cb36cd7910683870619ffac39c918
hussen2003 <hussenpremier03@gmail.com>
2024-04-10 13:14:28 -0400

    devices mac addresses

--- src/getMacAddress.ino
+++ src/getMacAddress.ino
@@ -6,7 +6,8 @@ void setup()
   Serial.print("\nDefault ESP32 MAC Address: ");
   Serial.println(WiFi.macAddress());
 }
-
+// dev1 MAC Address: E4:65:B8:76:88:B0
+// dev2 MAC Address: MAC Address: FC:B4:67:72:7C:94
 void loop()
 {
   // Do Nothing
